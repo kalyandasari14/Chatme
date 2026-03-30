3df4cd2a9b24f379d11a9569b239509aaee5d5c8
kalyandasari14 <kalyandasari80@gmail.com>
2026-03-30 09:34:19 -0500

    Initial Commit

---
+++ Chatme/ChatmeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatmeApp.swift
+//  Chatme
+//
+//  Created by kalyan on 3/30/26.
+//
+
+import SwiftUI
+
+@main
+struct ChatmeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
